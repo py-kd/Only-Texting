e43b4b9c0306c5062e69391533f8f5e4b159a809
patel <patelbapu2000@gmail.com>
2024-05-08 10:10:38 +0530

    Drag & Drop Functionality using pure html, css & js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,30 @@
-document.title="Testing Only"
+document.title="Drag & Drop Functionality"
+
+let items = document.getElementsByClassName("item");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for (let item of items) {
+      item.addEventListener("dragstart", (e)=>{
+            let selected = e.target;
+            
+            rightBox.addEventListener("dragover", (e)=>{
+                  e.preventDefault();
+            });
+            
+            rightBox.addEventListener("drop", (e)=>{
+                  rightBox.appendChild(selected);
+                  selected=null;
+            });
+            
+            leftBox.addEventListener("dragover", (e)=>{
+                  e.preventDefault();
+            });
+            
+            leftBox.addEventListener("drop", (e)=>{
+                  leftBox.appendChild(selected);
+                  selected=null;
+            });
+
+      });
+}
