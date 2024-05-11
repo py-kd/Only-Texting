085fd8c25bb1ecf1dbe332279c654b3625daacd9
patel <patelbapu2000@gmail.com>
2024-05-11 23:07:42 +0530

    Responsive UI of ToDo app

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+let inputElement = document.getElementById("myDateInput");
+console.log(inputElement);
+
+let insertBtn = document.getElementById("insertBtn");
+console.log(insertBtn);
+
+let listBox = document.querySelector(".list-box");
+console.log(listBox);
