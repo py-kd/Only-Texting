044e305f4d996a39591ff12526965ce3a8c6ba5d
patel <patelbapu2000@gmail.com>
2024-06-22 12:26:54 +0530

    OWL Carousel scroll on MouseScroll added by jQueryMouseWheel

--- assets/script.js
+++ assets/script.js
@@ -26,4 +26,14 @@ $(document).ready(function() {
                 }
             }
         })
+
+      var owl = $('.owl-carousel');
+      owl.on('mousewheel', '.owl-stage', function (e) {
+            if (e.deltaY>0) {
+                  owl.trigger('next.owl');
+            } else {
+                  owl.trigger('prev.owl');
+            }
+            e.preventDefault();
+      });
 });
