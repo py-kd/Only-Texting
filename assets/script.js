6c00c10eb41eb7d5281cf37d984d79a3c5e414ff
patel <patelbapu2000@gmail.com>
2024-07-23 09:17:16 +0530

    Element remove on click

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,9 @@ mylist.addEventListener('click', (e) => {
       // console.log(e.bubbles);
       // console.log(e.CAPTURING_PHASE);
       // console.log(e.target);
+      console.log(e.target.tagName);
+      console.log(e.target.id);
+      // console.log(e.target.parentNode);
       // console.log(e.currentTarget);
       // console.log(e.type);
       // console.log(e.defaultPrevented);
@@ -20,17 +23,24 @@ mylist.addEventListener('click', (e) => {
       // console.log(`TILT -> from left:: ${e.tiltX} from top:: ${e.tiltY}`);
       // console.log(`WINDOW -> from left:: ${e.tiltX} from top:: ${e.tiltY}`);
 
-      // console.log(mylist.attributes);
+      // console.log(mylist.attributes); 
       // console.log(mylist.attributes[0]);
 
-      console.log(`container clicked`);
-      e.stopPropagation();
+      // console.log(`container clicked`);
+      // e.stopPropagation();
+
+      e.target.parentNode.removeChild(e.target);
 }, false);
 
+// listItem.addEventListener('click',(e) => {
+//       console.log(`item clicked`);
+//       e.stopPropagation();
+//       e.stopPropagation();
+// }, true);
 
-listItem.addEventListener('click',(e) => {
-      console.log(`item clicked`);
-      e.stopPropagation();
-      e.stopPropagation();
-}, true);
-
+document.addEventListener('DOMContentLoaded', (e) => {
+      console.log(`dom loaded`);
+});
+document.addEventListener('', (e) => {
+      console.log(`dom resize`);
+});
